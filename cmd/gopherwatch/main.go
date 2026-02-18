0b6e7e9f534605d00fdde2c3bbf970d38633cea3
Khandakerzoadpial <khpial27@gmail.com>
2026-02-18 21:35:38 +0600

    watcher logic implemented and tested in main

--- cmd/gopherwatch/main.go
+++ cmd/gopherwatch/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"gopherwatch/internal/watcher"
+	"log"
+)
+
+func main() {
+	watcher := &watcher.FileWatcher{FileName: "test.log"}
+
+	logs, err := watcher.Watch()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Watching test.log... append some text to see it!")
+
+	for line := range logs {
+		fmt.Print("New Log: ", line)
+	}
+}
--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -1,25 +1,58 @@
 package watcher
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 type Watcher interface {
-	Watch() (chan string, error)
+	Watch() (<-chan string, error)
 }
 
 type FileWatcher struct {
 	FileName string
 }
 
-func (wh *FileWatcher) Watch() (chan string, error) {
-	var file, err = os.Open(wh.FileName)
+func (fw *FileWatcher) run(file *os.File, ch chan string) {
+	defer file.Close()
+	defer close(ch)
+
+	var reader = bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		if err != nil {
+			if err == io.EOF {
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+
+			fmt.Printf("Watcher Error On: %s:%v\n", fw.FileName, err)
+			return
+		}
+		ch <- line
+	}
+}
+
+func (fw *FileWatcher) Watch() (chan string, error) {
+	var file, err = os.Open(fw.FileName)
 
 	if err != nil {
 		return nil, err
 	}
 
-	var endLine = file.Seek(0, io.SeekEnd)
+	_, err = file.Seek(0, io.SeekEnd)
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
 
+	var channel = make(chan string)
+	go fw.run(file, channel)
+
+	return channel, nil
 }
