4e1d07b538f6729ac5b037773524562908167582
Khandakerzoadpial <khpial27@gmail.com>
2026-02-21 20:29:39 +0600

    implemented

--- cmd/gopherwatch/main.go
+++ cmd/gopherwatch/main.go
@@ -2,11 +2,52 @@ package main
 
 import (
 	"fmt"
+	"gopherwatch/internal/hub"
 	"gopherwatch/internal/watcher"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func serveWS(h *hub.Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	clientChannel := make(chan string)
+	h.Register <- clientChannel
+
+	defer func() {
+		h.Unregister <- clientChannel
+	}()
+
+	for {
+		message, ok := <-clientChannel
+		if !ok {
+			break
+		}
+		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			break
+		}
+	}
+
+}
+
 func main() {
+	hub := hub.NewHub()
+	go hub.Run()
+
 	watcher := &watcher.FileWatcher{FileName: "test.log"}
 
 	logs, err := watcher.Watch()
@@ -16,7 +57,21 @@ func main() {
 	}
 	fmt.Println("Watching test.log... append some text to see it!")
 
-	for line := range logs {
-		fmt.Print("New Log: ", line)
+	go func() {
+		for line := range logs {
+			hub.Broadcast <- line
+		}
+	}()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(hub, w, r)
+	})
+
+	http.Handle("/", http.FileServer(http.Dir("../../ui")))
+
+	fmt.Println("GopherWatch live on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
+
 }
--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -3,7 +3,35 @@ package hub
 type Hub struct {
 	clients map[chan string]bool
 
-	Register   chan string
-	Unregister chan string
+	Register   chan chan string
+	Unregister chan chan string
 	Broadcast  chan string
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[chan string]bool),
+		Register:   make(chan chan string),
+		Unregister: make(chan chan string),
+		Broadcast:  make(chan string),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.Register:
+			h.clients[client] = true
+		case client := <-h.Unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client)
+			}
+		case message := <-h.Broadcast:
+			for client := range h.clients {
+				client <- message
+			}
+
+		}
+	}
 }
